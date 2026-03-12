c646d518395ff66e6c35862b30117de5359b62f4
Thaiany Domingues <tmdm@cin.ufpe.br>
2026-03-12 19:01:30 +0000

    Add photo thumbnails, elapsed timer and mobile install option

---
+++ web/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'observa-pe-v1';
+const ASSETS = ['/', '/manifest.json', '/icon.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match('/'));
+    })
+  );
+});
